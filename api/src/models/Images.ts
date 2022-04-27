9005a1b419e8d77a5e671b257979f266bb77cb2a
Yussuf Sassi <yussufsassi@gmail.com>
2022-04-27 13:27:48 +0200

    api - initial build
    NOT final build. Only MVP

---
+++ api/src/models/Images.ts
@@ -0,0 +1,20 @@
+import { Schema, model, connect } from 'mongoose';
+
+interface image{
+    id: string,
+    filename: string,
+    size: number,
+    uploadedAt: Date,
+    path: string
+}
+
+const imageSchema = new Schema<image>({
+    id: {type: String, required: true},
+    filename: {type: String, required: true},
+    size: {type: Number, required: true},
+    uploadedAt: {type: Date, required: true},
+    path: {type: String, required: true}
+})
+
+const Image = model<image>("Image", imageSchema);
+export default Image;
